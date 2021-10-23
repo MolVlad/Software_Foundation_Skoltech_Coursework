838761171bb687ca59fb59eeff4d3d2d25dc024e
Vlad <molvlad@frtk.ru>
2021-10-23 18:56:20 +0300

    Add entrypoint for download.sh

--- docker/download/download.sh
+++ docker/download/download.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_train.zip
+#wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_train.zip
+#unzip 3dmv_scannet_v2_train.zip -d train
+
 wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_test_scenes.zip
+unzip 3dmv_scannet_v2_test_scenes.zip -d test
 
 
