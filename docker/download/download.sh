7e3d4b3c0c385ad2d7108ff38a0b760b48b10988
Vlad <molvlad@frtk.ru>
2021-10-23 20:53:16 +0300

    Finish Dockerfile for download step

--- docker/download/download.sh
+++ docker/download/download.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir data
+
 #wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_train.zip
 #unzip 3dmv_scannet_v2_train.zip -d train
 
 wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_test_scenes.zip
-unzip 3dmv_scannet_v2_test_scenes.zip -d test
-
+unzip 3dmv_scannet_v2_test_scenes.zip -d data/test
+rm 3dmv_scannet_v2_test_scenes.zip
 
