4f2b579851589c317c930229f8b7ab89b0cb6eeb
MolVlad <Molvlad@frtk.ru>
2021-10-26 22:58:39 +0300

    add flags

--- docker/eval/test.sh
+++ docker/eval/test.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 python2 unit_test.py
-python2 test.py
+python2 test.py $TEST_FLAGS
 
--- docker/train/train.sh
+++ docker/train/train.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 python2 unit_test.py
-python2 train.py
+python2 train.py $TRAIN_FLAGS
 
