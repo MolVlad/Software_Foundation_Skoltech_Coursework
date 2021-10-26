5df6e01bd4f9994eb6e4afb77bc1f6ebea4dafdd
MolVlad <Molvlad@frtk.ru>
2021-10-26 18:26:34 +0300

    Fix destination directory

--- docker/download/download.sh
+++ docker/download/download.sh
@@ -2,6 +2,10 @@
 
 mkdir -p data/scannetv2 data/scannetv2_images
 
+wget http://kaldir.vc.in.tum.de/adai/3DMV/models.zip
+unzip models.zip -d data
+rm models.zip
+
 wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_train.zip
 unzip 3dmv_scannet_v2_train.zip -d data/train
 rm 3dmv_scannet_v2_train.zip
