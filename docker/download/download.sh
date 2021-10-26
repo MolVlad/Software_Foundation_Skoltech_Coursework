a1ff66c8d67d812d1e4dad5becbaf65b63aa73bc
MolVlad <Molvlad@frtk.ru>
2021-10-26 17:35:28 +0300

    Change directory to store train data

--- docker/download/download.sh
+++ docker/download/download.sh
@@ -3,7 +3,7 @@
 mkdir -p data/scannetv2 data/scannetv2_images
 
 wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_train.zip
-unzip 3dmv_scannet_v2_train.zip -d train
+unzip 3dmv_scannet_v2_train.zip -d data/train
 rm 3dmv_scannet_v2_train.zip
 
 wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_test_scenes.zip
