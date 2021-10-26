fae2d674e932f77de911cf0614353ef6455fffc5
Vlad <molvlad@frtk.ru>
2021-10-26 18:51:15 +0300

    Change destinations

--- docker/download/download.sh
+++ docker/download/download.sh
@@ -7,10 +7,10 @@ unzip models.zip -d data
 rm models.zip
 
 wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_train.zip
-unzip 3dmv_scannet_v2_train.zip -d data/train
+unzip 3dmv_scannet_v2_train.zip -d data/3dmv_scannet_v2_train
 rm 3dmv_scannet_v2_train.zip
 
 wget http://kaldir.vc.in.tum.de/adai/3DMV/data/3dmv_scannet_v2_test_scenes.zip
-unzip 3dmv_scannet_v2_test_scenes.zip -d data/test
+unzip 3dmv_scannet_v2_test_scenes.zip -d data/3dmv_scannet_v2_test_scenes
 rm 3dmv_scannet_v2_test_scenes.zip
 
